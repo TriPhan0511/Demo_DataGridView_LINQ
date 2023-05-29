8d0de399390e9e39a9f70c92b2794d4b0398da3e
Tri Phan <phanphutri@gmail.com>
2023-05-29 20:16:54 +0700

    Updated

--- Demo_DataGridView_LINQ/Form1.Designer.cs
+++ Demo_DataGridView_LINQ/Form1.Designer.cs
@@ -29,13 +29,110 @@ namespace Demo_DataGridView_LINQ
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(540, 332);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(10, 350);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(75, 23);
+            this.btnView.TabIndex = 1;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(91, 350);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(253, 350);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(172, 350);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(334, 353);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(137, 20);
+            this.txtKey.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(477, 350);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(581, 413);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Button btnSearch;
     }
 }
 
--- Demo_DataGridView_LINQ/Form1.cs
+++ Demo_DataGridView_LINQ/Form1.cs
@@ -15,6 +15,85 @@ namespace Demo_DataGridView_LINQ
         public Form1()
         {
             InitializeComponent();
+
         }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            using (MyDataClassesDataContext db = new MyDataClassesDataContext())
+            {
+                dataGridView1.DataSource = from emp in db.Employees
+                                           select emp;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (MyDataClassesDataContext db = new MyDataClassesDataContext())
+            {
+
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // TESTING
+        //private void btnView_Click(object sender, EventArgs e)
+        //{
+        //    using (MyDataClassesDataContext db = new MyDataClassesDataContext())
+        //    {
+        //        dataGridView1.DataSource = from emp in db.Employees
+        //                                   select emp;
+
+        //        //dataGridView1.DataSource = db.Employees.Select(p => p);
+        //        //dataGridView1.DataSource =
+        //        //    db.Employees
+        //        //    .Where(p => p.Id > 1)
+        //        //    .OrderBy(p => p.Sex);
+
+        //        //dataGridView1.DataSource =
+        //        //    from emp in db.Employees
+        //        //    where emp.Id > 1
+        //        //    orderby emp.Id descending
+        //        //    select emp;
+
+        //        //dataGridView1.DataSource =
+        //        //    from emp in db.Employees
+        //        //    from profile in db.EmployeeProfiles
+        //        //    where emp.Id == profile.EmployeeId
+        //        //    select profile;
+
+        //        //dataGridView1.DataSource =
+        //        //    from emp in db.Employees
+        //        //    from profile in db.EmployeeProfiles
+        //        //    where emp.Id == profile.EmployeeId
+        //        //    select new { 
+        //        //        Id = emp.Id,
+        //        //        EmployeeName = emp.Name,
+        //        //        Info = profile.Info
+        //        //    };
+
+        //        //dataGridView1.DataSource =
+        //        //    from emp in db.Employees
+        //        //    from profile in db.EmployeeProfiles
+        //        //    where emp.Id == profile.EmployeeId
+        //        //    select new
+        //        //    {
+        //        //        Id = emp.Id,
+        //        //        EmployeeName = emp.Name,
+        //        //        Info = profile.Info
+        //        //    } into newSelect 
+        //        //        where newSelect.Id == 1 
+        //        //        select newSelect;
+        //    }
+        //}
     }
 }
